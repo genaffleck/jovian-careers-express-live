ae5f978c4de274c6c0d5c40b72b73966a6f546f5
genaffleck <97855765+genaffleck@users.noreply.github.com>
2023-12-10 17:34:07 +0000

    configured mustache

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const JOBS = require('./jobs');
+const mustacheExpress = require('mustache-express');
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Configure mustache
+app.set('views', path.join(__dirname,'pages'));
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+
+
 app.get('/', (req, res) => {
     // res.send('Hello World!');
-    res.sendFile(path.join(__dirname, 'pages/index.html'));
+    // res.sendFile(path.join(__dirname, 'pages/index.html'));
+    res.render('index', {jobs: JOBS});
 });
 
 const port = process.env.PORT || 3000;
